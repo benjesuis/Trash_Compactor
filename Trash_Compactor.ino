53e0d458a7f81ebada8c364a01ef32651ffad2bb
benjesuis <32913438+benjesuis@users.noreply.github.com>
2018-11-26 16:57:04 -0500

    Add main program

--- Trash_Compactor.ino
+++ Trash_Compactor.ino
@@ -1,3 +1,107 @@
+/*  
+ *   Boston University
+ *   EK 210 Section E1
+ *   Team Bronze: Trash Compactor
+ *   Written By: Ben Livney
+*/
+
+#include <HX711.h>
+const int motorPin = 5;
+const int valvePin = 6;
+const int lockPin = 9;
+const int distPin = A0;
+const int switchOut = A1;
+const int switchIn = A2;
+const int led = A3;
+HX711 scale(3, 2);
+
+int distance;
+bool switched;
+unsigned long timer;
+unsigned long zeroTimer;
+
+void poll();
+void compact();
+void reset();
+
+void setup() {
+  Serial.begin(9600);
+  switched = false;
+  distance = -1;
+  pinMode(motorPin, OUTPUT);
+  pinMode(valvePin, OUTPUT);
+  pinMode(lockPin, OUTPUT);
+  scale.set_scale();
+  scale.tare();
+}
+
+void loop() {
+  timer = millis();
+  analogWrite(switchOut, 255);
+  poll();
+  while (switched && (timer - zeroTimer) > (1000 * 60 * 2.5)) {
+    timer = millis();
+    analogWrite(switchOut, 255);
+    analogWrite(led, 255);
+    compact();
+  }
+  reset();
+}
+
+void poll() {
+  scale.set_scale(7050);
+  Serial.println(scale.get_units(), 1);
+  distance = analogRead(distPin);
+  if (distance > 350) {
+    analogWrite(led, 255);
+  }
+  if (analogRead(switchIn) > 200) {
+    switched = true;
+    zeroTimer = timer;
+  }
+}
+
+void compact() {
+  digitalWrite(motorPin, HIGH);
+  digitalWrite(lockPin, HIGH);
+}
+
+void reset() {
+  digitalWrite(motorPin, LOW);
+  digitalWrite(valvePin, HIGH);
+  delay(1000 * 60 * 2.5);
+  while(switched);
+  digitalWrite(lockPin, LOW);
+}
+
+
+
+/* MOTOR */
+//const int motorPin = 3;
+//const int valvePin = 5;
+//const int lockPin = 6;
+//
+//void setup() {
+//  pinMode(motorPin, OUTPUT);
+//  pinMode(valvePin, OUTPUT);
+//  pinMode(lockPin, OUTPUT);
+//
+//  pinMode(LED_BUILTIN, OUTPUT);
+//  Serial.begin(9600);
+//}
+//
+//void loop() {
+//  digitalWrite(motorPin, HIGH);
+//  digitalWrite(valvePin, HIGH);
+//  digitalWrite(lockPin, HIGH);
+//  digitalWrite(LED_BUILTIN, HIGH);
+//  delay(500);
+//  digitalWrite(motorPin, LOW);
+//  digitalWrite(valvePin, LOW);
+//  digitalWrite(lockPin, LOW);
+//  digitalWrite(LED_BUILTIN, LOW);
+//  delay(1500);
+//}
 
 
 /* LOAD CELL */
@@ -5,7 +109,7 @@
 //#define DOUT  3
 //#define CLK  2
 //
-//float calibrationFactor = -7050;
+//float calibrationFactor = 70.5;
 //HX711 scale(DOUT, CLK);
 //
 //void setup() {
@@ -13,9 +117,7 @@
 //  scale.set_scale();
 //  scale.tare();
 //  long zeroFactor;
-//  do {
-//    zeroFactor = scale.read_average();
-//  } while (zeroFactor != -1);
+//  zeroFactor = scale.read_average();
 //  Serial.println(zeroFactor);
 //}
 //
@@ -24,12 +126,12 @@
 //  Serial.println(scale.get_units(), 1);
 //}
 
+
 /* IR SENSOR */
 //const int distPin = A0;
 //
 //void setup() {
 //  Serial.begin(9600);
-//  pinMode(outPin, OUTPUT);
 //}
 //
 //void loop() {
@@ -39,5 +141,5 @@
 //    Serial.println("Compact");
 //  } else {
 //    Serial.println(distance);
-//  } 
+//  }
 //}
